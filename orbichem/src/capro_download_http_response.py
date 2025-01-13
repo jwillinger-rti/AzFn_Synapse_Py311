644df467ba824ead35f1fd6744565773619cb07b
Jonathan Willinger <jwillinger@rtiglobal.com>
2025-01-13 14:55:49 -0600

    Add capro from orbichem

---
+++ orbichem/src/capro_download_http_response.py
@@ -0,0 +1,98 @@
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContentSettings
+from azure.identity import DefaultAzureCredential
+from azure.core import exceptions
+import logging, os, sys
+import pathlib as path
+import inspect, json
+import datetime, tempfile
+try: import orbichem.src.pull_orbichem_data as pull_orbichem
+except ModuleNotFoundError: import pull_orbichem_data as pull_orbichem
+
+
+PROJECT_DIR = path.Path(__file__).parent.parent.parent
+
+def get_and_config_logger(log_file):
+
+    # log_dir = os.path.join(path.Path(__file__).parent.parent.resolve()
+    # os.makedirs(log_dir, exist_ok=True)
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.INFO)
+    # handler = logging.StreamHandler(stream=sys.stdout)
+    # formatter = logging.Formatter(fmt='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s')
+    # handler.setFormatter(formatter)
+    # logger.addHandler(handler)
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    with tempfile.NamedTemporaryFile(mode='a', delete=False) as temp_file:
+        temp_file_name = temp_file.name
+    file_handler = logging.FileHandler(temp_file_name)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger, temp_file_name
+
+def upload_log_to_blob(logger, temp_file_name, adls_conn_string):
+
+    container_name = "synapse-fn-logs"
+    blob_name = f"{logger.name}_{datetime.datetime.now().strftime('%Y-%m-%d')}.txt"    
+    adls_svc_client = BlobServiceClient.from_connection_string(adls_conn_string)
+    container_client = adls_svc_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+    blob_exists = blob_client.exists()
+    
+    if not blob_exists:
+        with open(temp_file_name, "rb") as temp_file:
+            container_client.upload_blob(name=blob_name, data=temp_file, content_settings=ContentSettings(content_type='text/plain'))
+        print(f"Blob '{blob_name}' created with content from the temporary file.")
+    else:
+        existing_content = blob_client.download_blob().readall().decode('utf-8')
+        with open(temp_file_name, "r") as temp_file:
+            new_content = existing_content + temp_file.read()
+        blob_client.upload_blob(data=new_content, overwrite=True)
+
+def capro_download_http_reponse():
+    func_name = inspect.currentframe().f_code.co_name
+    logger, temp_file_name = get_and_config_logger(func_name)
+    
+    try:
+        with open(os.path.join(PROJECT_DIR,"local.settings.json")) as f:
+            data = json.load(f)
+            host = data["Values"]["SYNAPSE_INSTANCE"]
+            orbichem_uid = data["Values"]["ORBICHEM_UID"]
+            orbichem_pw = data["Values"]["ORBICHEM_PW"]
+            adls_conn_string = data["Values"]["WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"]
+            storage_account_key_for_synapse = data["Values"]["ADLS_STORAGEACCOUNTKEY_FORSYNAPSE"]
+            storage_account_name_for_synapse = data["Values"]["ADLS_STORAGEACCOUNTNAME_FORSYNAPSE"]
+
+    except FileNotFoundError or FileNotFoundError or KeyError:
+        host = os.environ["SYNAPSE_INSTANCE"]
+        orbichem_uid = os.environ["ORBICHEM_UID"]
+        orbichem_pw = os.environ["ORBICHEM_PW"]
+        adls_conn_string = os.environ["WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"]
+        storage_account_key_for_synapse = data["Values"]["ADLS_STORAGEACCOUNTKEY_FORSYNAPSE"]
+        storage_account_name_for_synapse = data["Values"]["ADLS_STORAGEACCOUNTNAME_FORSYNAPSE"]
+
+    try:
+        # host1 = "rti-synapse-db.sql.azuresynapse.net" # SBX
+        # host2 = "rti-synapse-pd.sql.azuresynapse.net" # PRD
+        orb = pull_orbichem.orbichem(host, orbichem_uid, orbichem_pw, storage_account_key_for_synapse, storage_account_name_for_synapse)
+        orb.main_capro(storage_account_name=storage_account_key_for_synapse, storage_account_key=storage_account_key_for_synapse, 
+                       orbichem_uid=orbichem_uid, orbichem_pw=orbichem_pw)
+    
+    except Exception as e:
+        logger.error(e)
+        logger.error("run failed. \n")
+        b_success = False
+    else:
+        logger.info("run successful. \n")
+        b_success = True
+    
+    upload_log_to_blob(logger, temp_file_name, adls_conn_string)
+    
+    return b_success
+
+if __name__ == "__main__":
+    capro_download_http_reponse()
---
+++ orbichem/src/pull_orbichem_data.py
@@ -0,0 +1,114 @@
+# Capro:
+# https://orbichem360.orbichem.com/price/monitor/
+# Tecnon Orbichem
+
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+from azure.storage.blob import BlobServiceClient, ContentSettings
+
+class orbichem():
+    
+    def __init__(self, host, orbichem_uid, orbichem_pw, 
+                 storage_account_key_for_synapse, storage_account_name_for_synapse):
+        # URLs
+        self.capro_login_url = "https://orbichem360.orbichem.com/auth/signin?from_page=/"
+        self.capro_url = "https://orbichem360.orbichem.com/price/monitor/get_price_data"
+        self.host = host
+        self.orbichem_uid = orbichem_uid
+        self.orbichem_pw = orbichem_pw
+        self.storage_account_key_for_synapse = storage_account_key_for_synapse
+        self.storage_account_name_for_synapse = storage_account_name_for_synapse
+
+    def upload_dataframe_to_azure_blob(self, dataframe, directory, file_name):
+
+        csv_data = dataframe.to_csv(index=False)
+        container_name = "rti-synapse-db"
+        blob_name = f"{directory}/{file_name}"
+        
+        blob_service_client = BlobServiceClient(account_url=f"https://{self.storage_account_name}.blob.core.windows.net",
+                                                credential=self.storage_account_key)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.upload_blob(csv_data, blob_type="BlockBlob", overwrite=True, content_settings=ContentSettings(content_type='text/csv'))
+
+    def main_capro(self, storage_account_name, storage_account_key, orbichem_uid, orbichem_pw):
+        
+        username = orbichem_uid
+        password = orbichem_pw
+        # Current date and time
+        current_date = datetime.now()
+
+        # First day of the current month
+        first_day_of_current_month = current_date.replace(day=1).strftime('%m/%d/%Y')
+
+        # First day of the previous month
+        first_day_previous_month = (current_date.replace(day=1) - timedelta(days=1)).replace(day=1)
+        first_day_previous_month_formatted = first_day_previous_month.strftime('%Y-%m-%d')
+
+        with requests.Session() as session:
+            # Login to the website
+            login_payload = {
+                'username': username,
+                'password': password
+            }
+            response = session.post(self.capro_login_url, data=login_payload)
+
+            # Define request headers and data
+            headers = {
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "en-US,en;q=0.9,uz;q=0.8",
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-requested-with": "XMLHttpRequest"
+            }
+
+            data = {
+                "start_date": "01/01/2014",
+                "end_date": f"{first_day_of_current_month}",
+                "currency": "USD",
+                "cbf_group": "9",
+                "chemical[]": "20",
+                "uom_id": "0",
+                "regions[]": ["9"],  # 9 - China
+                "definitions[]": ["1129"], # Domestic Spot
+                "panel_states[dv_cont_1][data]": "",
+                "panel_states[dv_cont_1][time_scale]": "M",
+                "panel_states[dv_cont_4][data]": "",
+                "panel_states[dv_cont_4][time_scale]": "",
+                "product_id": "20" # Caprolactam
+            }
+
+            # Send POST request to fetch data
+            response = session.post(self.capro_url, headers=headers, data=data)
+            response.raise_for_status()  # Raise an error for bad response status codes
+
+            # Parse JSON response
+            json_data = response.json()
+
+            # Find matching entry for the first day of the previous month
+            matching_entries = [entry for entry in json_data['price_data'] if entry['date'] == first_day_previous_month_formatted]
+
+            # Create DataFrame from matching entry
+            if matching_entries:
+                capro_df = pd.DataFrame(matching_entries[0], index=[0])
+                capro_df = capro_df[['date', 'name', 'region', 'definition', 'primary_low', 'primary_high', 'converted_low', 'converted_high', 'price']]
+                capro_df = capro_df.rename(columns={'date': 'price_date'})
+                # capro_df['load_date'] = today
+
+
+        directory = "drivers-web-data/capro"
+        file_name = f"capro_{first_day_previous_month.strftime('%Y%m%d')}.csv"
+        self.upload_dataframe_to_azure_blob(storage_account_name, storage_account_key, dataframe=capro_df,
+                directory=directory, file_name=file_name)
+
+
+if __name__ == "__main__":
+
+    orb = orbichem()
+    orb.main_capro(orb.storage_account_name_for_synapse, orb.storage_account_key_for_synapse,
+                   orb.orbichem_uid, orb.orbichem_pw)
